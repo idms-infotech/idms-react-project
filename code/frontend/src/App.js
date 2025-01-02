82728e83f9faa36e2897397efb8653ef9e384bd1
Aaliya9 <aaliyasharif1998@gmail.com>
2025-01-02 14:50:21 +0530

    assets added

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <Router>
