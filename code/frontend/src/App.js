500edbf67d53d2bbe92f562632234aaf64406d32
idms-infotech <idmsqa5@gmail.com>
2025-01-02 14:50:40 +0530

    sccsss

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/styles/styles.scss';
 
 function App() {
   return (
